d96bb881342ea494f16fab1c88d78f549afb5314
dantedanette0 <carminedante@yahoo.com>
2025-05-30 22:54:08 -0400

    added random colors

--- main.js
+++ main.js
@@ -15,29 +15,38 @@ function create_grid(rows, columns) {
   }
 }
 
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
 function remove_grid() {
   const rows = container.querySelectorAll('.row');
   rows.forEach(row => row.remove());
 }
 
-
 container.addEventListener('mouseover', e => {
-if (e.target.matches('.cell')) {
+  if (e.target.matches('.cell')) {
     e.target.style.backgroundColor = 'blue';
-}
+  }
 });
 
 container.addEventListener('mouseout', e => {
-if (e.target.matches('.cell')) {
-    e.target.style.backgroundColor = 'purple';
-}
+  if (e.target.matches('.cell')) {
+  
+    e.target.style.backgroundColor = getRandomColor();
+  }
 });
 
-const new_sktech_button = document.querySelector('.new-sketch');
-
-
-new_sktech_button.addEventListener('click', () => {
-    remove_grid();
-    create_grid(128,128);
+const new_sketch_button = document.querySelector('.new-sketch');
+new_sketch_button.addEventListener('click', () => {
+  remove_grid();
+  create_grid(128, 128);
 });
+
+
+create_grid(16, 16);
